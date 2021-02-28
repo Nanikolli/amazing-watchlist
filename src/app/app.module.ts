599e6ee13f8644a9104165f01fbf9e16a14ed9ca
Nanikolli <nagarjunareddykolli345@gmail.com>
2021-02-28 14:14:21 -0500

    Added show-info content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,26 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchComponent } from './search/search.component';
-import { ShowDetailsComponent } from './show-details/show-details.component';
+import { SearchResultsComponent } from './search-results/search-results.component';
 import { ShowInfoComponent } from './show-info/show-info.component';
+import { ShowDetailsComponent } from './show-details/show-details.component';
 import { WatchlistComponent } from './watchlist/watchlist.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatInputModule} from '@angular/material/input';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SearchComponent,
-    ShowDetailsComponent,
+    SearchResultsComponent,
     ShowInfoComponent,
+    ShowDetailsComponent,
     WatchlistComponent
   ],
   imports: [
@@ -31,7 +36,8 @@ import {MatIconModule} from '@angular/material/icon';
     MatInputModule,
     FormsModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
